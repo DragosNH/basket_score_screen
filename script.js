1411fb532f7ae13e5cfdb156ccdd685bb7dd8978
Dragoş <edragos33@gmail.com>
2026-02-04 18:50:13 +0100

    buttons declared

---
+++ script.js
@@ -0,0 +1,13 @@
+// --------- Declare variables ---------
+
+// ------ Buttons ------
+
+// --- Home buttons ---
+const home1p = document.querySelector(".home1p");
+const home2p = document.querySelector(".home2p");
+const home3p = document.querySelector(".home3p");
+
+// --- Visitors buttons ---
+const visitors1p = document.querySelector(".visitors1p");
+const visitors2p = document.querySelector(".visitors2p");
+const visitors3p = document.querySelector(".visitors3p");
